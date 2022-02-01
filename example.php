ccb2e6c19ad4a3c07875eb955e1b5c43c21b0e4f
NextSMS.cz <93512292+NextSMScz@users.noreply.github.com>
2022-02-01 19:14:09 +0100

    Add files via upload

--- example.php
+++ example.php
@@ -1,10 +1,39 @@
 <?php
 
+//Připojení knihovny do kódu
 require_once "nextsmsAPI.php";
 
+//Odeslání SMS ze systémového čísla
+$nextsms = new nextsmsAPI('Váš API Auth Token');
+try {
+	$nextsms->sendSMS('+420123456789', 'První API SMS ', 'system', '');
+	echo "Zpráva byla úspěšně odeslána.";
+} catch (nextsmsAPIException $ex) {
+	echo "Nastala chyba: ".$ex->getMessage();
+}
+
+//Odeslání SMS z Short code
+$nextsms = new nextsmsAPI('Váš API Auth Token');
+try {
+	$nextsms->sendSMS('+420123456789', 'První API SMS ', 'short', '');
+	echo "Zpráva byla úspěšně odeslána.";
+} catch (nextsmsAPIException $ex) {
+	echo "Nastala chyba: ".$ex->getMessage();
+}
+
+//Odeslání SMS z textového odesílatele
 $nextsms = new nextsmsAPI('Váš API Auth Token');
 try {
 	$nextsms->sendSMS('+420123456789', 'První API SMS ', 'text', 'NextSMS.cz');
+	echo "Zpráva byla úspěšně odeslána.";
+} catch (nextsmsAPIException $ex) {
+	echo "Nastala chyba: ".$ex->getMessage();
+}
+
+//Odeslání SMS z vlastního čísla - je potřeba ověření čísla, v případě zájmů nás kontaktujte na info@nextsms.cz
+$nextsms = new nextsmsAPI('Váš API Auth Token');
+try {
+	$nextsms->sendSMS('+420123456789', 'První API SMS ', 'own', '+420987654321');
 	echo "Zpráva byla úspěšně odeslána.";
 } catch (nextsmsAPIException $ex) {
 	echo "Nastala chyba: ".$ex->getMessage();
